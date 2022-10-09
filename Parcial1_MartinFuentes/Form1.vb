403e85a46bd8de56e55270db9662c1b54196200b
Martin-Fuentes <martinfuentes012@gmail.com>
2022-10-08 23:07:11 -0500

    Conexión con DB Establecida

--- Parcial1_MartinFuentes/Form1.vb
+++ Parcial1_MartinFuentes/Form1.vb
@@ -1,3 +1,22 @@
-﻿Public Class Form1
+﻿Imports System.Diagnostics.Eventing
+Imports MySql.Data.MySqlClient
+Public Class Form1
+    Private cmd As MySqlCommand
+    Private reader As MySqlDataReader
+    Public con As conexion
+    Private Sub Label5_Click(sender As Object, e As EventArgs) Handles Label5.Click
 
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        con = New conexion()
+        cmd = New MySqlCommand("Select * from cliente", con.getConexion())
+        reader = cmd.ExecuteReader()
+        If (IsNothing(reader)) Then
+            MsgBox("No se hizo la conexión", MsgBoxStyle.MsgBoxHelp, "Titulo")
+        Else
+            MsgBox("Conexión establecida", MsgBoxStyle.MsgBoxHelp, "Titulo")
+        End If
+
+    End Sub
 End Class
